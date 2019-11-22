9f02f24bd41b9980cceaa03b0e355d196a182fed
Alexey Vishentsev <alexey.vishentsev@gmail.com>
2019-11-22 23:24:22 +0300

    Updates

---
+++ precache-manifest.223b669431672fbd6be02d419f449079.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df34fbee74b2a8bdbc88a3e1e8a5fafc",
+    "url": "/multiply/index.html"
+  },
+  {
+    "revision": "6d869becf95d06e09bd9",
+    "url": "/multiply/static/css/main.33a3595d.chunk.css"
+  },
+  {
+    "revision": "d7d719c955c3743ecfde",
+    "url": "/multiply/static/js/2.6d801d44.chunk.js"
+  },
+  {
+    "revision": "6d869becf95d06e09bd9",
+    "url": "/multiply/static/js/main.266193a0.chunk.js"
+  },
+  {
+    "revision": "4a526cfa82b6f54279b6",
+    "url": "/multiply/static/js/runtime-main.c2488eb1.js"
+  }
+]);
