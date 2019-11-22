481e7a15e8364d963661ce3dd7db581d88c1690c
Alexey Vishentsev <alexey.vishentsev@gmail.com>
2019-11-22 23:22:49 +0300

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,82 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Умножалка</h1>
+      распечатать, разрезать, решать по 1 карточке в минуту.
+      <table className="App">
+        <tbody>
+          <tr>
+            <td>
+              <RandomTable/>
+            </td>
+            <td>
+              <RandomTable/>
+            </td>
+          </tr>
+
+          <tr>
+            <td>
+              <RandomTable/>
+            </td>
+            <td>
+              <RandomTable/>
+            </td>
+          </tr>
+
+          </tbody>
+        </table>
+    </>
   );
+};
+
+function randomOperation(): string {
+  let a = Math.floor(2.0 + Math.random()*8);
+  let b = Math.floor(2.0 + Math.random()*8);
+  let op;
+  if (Math.random() < 0.5) {
+    op = ' &centerdot; '
+  } else {
+    a = a*b;
+    op = ' : '
+  }
+  return `${a}${op}${b}`
 }
+
+function randomOperations(): string[][] {
+  let result = [];
+  for (let i = 0; i < 10; i++) {
+    result.push([randomOperation(), randomOperation(), randomOperation()])
+  }
+  return result;
+}
+
+
+const RandomTable: React.FC = () => {
+
+  const ops = randomOperations();
+  const rows = ops.map( r => {
+    let operations = r.map( o =>
+             (<>
+               <td className="op inner-td" dangerouslySetInnerHTML={{__html:o}}></td>
+               <td className="inner-td"></td>
+             </>));
+    return (
+      <tr>
+        {operations}
+      </tr>
+    );
+  });
+
+
+  return (<table className="randomTable">
+    <tbody>
+      {rows}
+    </tbody>
+  </table>)
+
+};
 
 export default App;
