e0b76d921d0b1644a4500f43ef8ddfa57f97d943
Alexey Vishentsev <alexey.vishentsev@gmail.com>
2020-05-19 19:14:43 +0300

    Add addition/subtraction

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,88 @@
 import React from 'react';
+import {
+  HashRouter as Router,
+  Link, Route, Switch,
+  useParams
+} from 'react-router-dom';
 import './App.css';
 
 const App: React.FC = () => {
-  return (
-    <>
+  return <>
+    <Router>
       <h1>Умножалка</h1>
-      распечатать, разрезать, решать по 1 карточке в минуту.
-      <table className="App">
-        <tbody>
-          <tr>
-            <td>
-              <RandomTable/>
-            </td>
-            <td>
-              <RandomTable/>
-            </td>
-          </tr>
-
-          <tr>
-            <td>
-              <RandomTable/>
-            </td>
-            <td>
-              <RandomTable/>
-            </td>
-          </tr>
-
-          </tbody>
-        </table>
-    </>
-  );
+      <Link to={'/mult'}>Умн/Дел</Link> &nbsp;
+      <Link to={'/add'}>Cл/Выч</Link>
+      <br/>Распечатать, разрезать, решать по 1 карточке в минуту.
+      <Switch>
+        <Route path={'/:op'} component={MainTable}/>
+        <Route path={'/'} component={MainTable}/>
+      </Switch>
+    </Router>
+  </>;
 };
 
-function randomOperation(): string {
-  let a = Math.floor(2.0 + Math.random()*8);
-  let b = Math.floor(2.0 + Math.random()*8);
-  let op;
-  if (Math.random() < 0.5) {
-    op = ' &centerdot; '
+function randomOperation(operation:string): string {
+  let a,b,op;
+  if (operation === 'add') {
+    a = Math.floor(1.0 + Math.random()*9);
+    b = Math.floor(1.0 + Math.random()*9);
+    if (Math.random() < 0.5) {
+      op = '&nbsp;+&nbsp;'
+    } else {
+      a = a + b;
+      op = '&nbsp;-&nbsp;'
+    }
   } else {
-    a = a*b;
-    op = ' : '
+    a = Math.floor(2.0 + Math.random()*8);
+    b = Math.floor(2.0 + Math.random()*8);
+    if (Math.random() < 0.5) {
+      op = '&nbsp;&centerdot;&nbsp;'
+    } else {
+      a = a*b;
+      op = '&nbsp;:&nbsp;'
+    }
   }
   return `${a}${op}${b}`
 }
 
-function randomOperations(): string[][] {
+function randomOperations(op:string): string[][] {
   let result = [];
   for (let i = 0; i < 10; i++) {
-    result.push([randomOperation(), randomOperation(), randomOperation()])
+    result.push([randomOperation(op), randomOperation(op), randomOperation(op)])
   }
   return result;
 }
 
+const MainTable: React.FC = () => {
+  return (
+    <table className="App">
+      <tbody>
+      <tr>
+        <td>
+          <RandomTable />
+        </td>
+        <td>
+          <RandomTable/>
+        </td>
+      </tr>
+
+      <tr>
+        <td>
+          <RandomTable/>
+        </td>
+        <td>
+          <RandomTable/>
+        </td>
+      </tr>
+
+      </tbody>
+    </table>
+  );
+};
 
 const RandomTable: React.FC = () => {
-
-  const ops = randomOperations();
+  let {op} = useParams();
+  const ops = randomOperations(op);
   const rows = ops.map( r => {
     let operations = r.map( o =>
              (<>
